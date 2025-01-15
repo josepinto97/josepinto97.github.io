79c20bef13f69de2fa3c575d3c0d611acfe281fb
jose <josepintore1997@gmail.com>
2025-01-15 14:51:55 +0100

    comprasa sottomenu

---
+++ interiordesign/script.js
@@ -0,0 +1,30 @@
+/*----- VARIABILI GLOBALI -----*/
+
+let sottomenu_aperto = false;
+
+
+/*----- GESTIONE APERTURA SOTTOMENU -----*/
+
+function apri_menu() {
+
+    let icona_hamburger = document.getElementById('menu_hamburger');
+    let sottomenu = document.getElementById('menu_sottomenu');
+
+    if (!sottomenu_aperto) {
+        sottomenu.style.left = '0';
+        sottomenu_aperto = true;
+    }
+
+}
+
+function chiudi_menu() {
+
+    let icona_hamburger = document.getElementById('menu_hamburger');
+    let sottomenu = document.getElementById('menu_sottomenu');
+
+    if (sottomenu_aperto) {
+        sottomenu.style.left = '100%';
+        sottomenu_aperto = false;
+    }
+
+}
