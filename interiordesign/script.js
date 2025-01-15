41f0c94904ef1ea2a1dd1b570e41c3d00c75f53f
jose <josepintore1997@gmail.com>
2025-01-15 18:18:21 +0100

    inzio main

--- interiordesign/script.js
+++ interiordesign/script.js
@@ -9,8 +9,10 @@ function apri_menu() {
 
     let icona_hamburger = document.getElementById('menu_hamburger');
     let sottomenu = document.getElementById('menu_sottomenu');
+    let header = document.getElementById('header');
 
     if (!sottomenu_aperto) {
+        header.style.overflow = 'visible';
         sottomenu.style.left = '0';
         sottomenu_aperto = true;
     }
@@ -21,10 +23,16 @@ function chiudi_menu() {
 
     let icona_hamburger = document.getElementById('menu_hamburger');
     let sottomenu = document.getElementById('menu_sottomenu');
+    let header = document.getElementById('header');
 
     if (sottomenu_aperto) {
         sottomenu.style.left = '100%';
         sottomenu_aperto = false;
+
+/*         sottomenu.addEventListener('transitionend', function onTransitionEnd() {
+            header.style.overflow = 'hidden';
+            sottomenu.removeEventListener('transitionend', onTransitionEnd);
+        }); */
     }
 
 }
