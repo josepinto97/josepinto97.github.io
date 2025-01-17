8196d9515b4083c076f4cfb7c1e4523468b89795
jose <josepintore1997@gmail.com>
2025-01-17 12:46:13 +0100

    menu a comparsa

--- interiordesign/script.js
+++ interiordesign/script.js
@@ -12,8 +12,10 @@ function apri_menu() {
     let header = document.getElementById('header');
 
     if (!sottomenu_aperto) {
-        sottomenu.style.left = '0';
-        sottomenu.style.position = 'fixed';
+/*                 sottomenu.style.top = '100%';
+                sottomenu.style.bottom = 'unset';
+                sottomenu_aperto = true; */
+        sottomenu.classList.add('aperto');
         sottomenu_aperto = true;
     }
 
@@ -26,8 +28,11 @@ function chiudi_menu() {
     let header = document.getElementById('header');
 
     if (sottomenu_aperto) {
-        sottomenu.style.left = '100%';
-        sottomenu.style.position = 'absolute';
+/*                 sottomenu.style.bottom = '100%';
+                sottomenu.style.top = 'unset';
+                sottomenu.style.position = 'absolute';
+                sottomenu_aperto = false; */
+        sottomenu.classList.remove('aperto');
         sottomenu_aperto = false;
     }
 
