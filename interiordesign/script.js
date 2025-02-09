e92964ba9bc01fa88aad31f14079b25844d3d5a3
jose <josepintore1997@gmail.com>
2025-02-09 11:59:35 +0100

    modifiche menu

--- interiordesign/script.js
+++ interiordesign/script.js
@@ -10,16 +10,16 @@ function apri_menu() {
     let icona_hamburger = document.getElementById('menu_hamburger');
     let sottomenu = document.getElementById('menu_sottomenu');
     let header = document.getElementById('header');
+    let contenitore_header = document.getElementById('contenitore_header');
 
     if (!sottomenu_aperto) {
-/*                 sottomenu.style.top = '100%';
-                sottomenu.style.bottom = 'unset';
-                sottomenu_aperto = true; */
         sottomenu.classList.add('aperto');
         sottomenu_aperto = true;
-    }else{
+        document.body.style.overflow = 'hidden';
+    } else {
         sottomenu.classList.remove('aperto');
         sottomenu_aperto = false;
+        document.body.style.overflow = 'auto';
     }
 
 }
@@ -36,10 +36,10 @@ function chiudi_menu() {
     let header = document.getElementById('header');
 
     if (sottomenu_aperto) {
-/*                 sottomenu.style.bottom = '100%';
-                sottomenu.style.top = 'unset';
-                sottomenu.style.position = 'absolute';
-                sottomenu_aperto = false; */
+        /*                 sottomenu.style.bottom = '100%';
+                        sottomenu.style.top = 'unset';
+                        sottomenu.style.position = 'absolute';
+                        sottomenu_aperto = false; */
         sottomenu.classList.remove('aperto');
         sottomenu_aperto = false;
     }
@@ -55,8 +55,8 @@ var modal = document.getElementById("myModal");
 var images = document.querySelectorAll("main .macchina-singola .galleria img");
 var modalImg = document.getElementById("img01");
 
-images.forEach(function(img) {
-    img.onclick = function(){
+images.forEach(function (img) {
+    img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
     }
@@ -67,19 +67,19 @@ var span = document.getElementsByClassName("close")[0];
 
 // Quando l'utente clicca su <span> (x), chiudi il modal
 if (span) {
-    span.onclick = function() { 
+    span.onclick = function () {
         modal.style.display = "none";
     }
 }
 
 /*----- BLOCCO MENU NELLA PARTE SUPERIORE  E CHIUSURA QUANDO CLICCO FUORI -----*/
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const sottomenu = document.getElementById('menu_sottomenu');
     const icona_hamburger = document.getElementById('menu_hamburger');
 
     if (sottomenu_aperto && !sottomenu.contains(event.target) && !icona_hamburger.contains(event.target)) {
-        chiudi_menu();  
+        chiudi_menu();
         icona_hamburger.classList.remove('active'); // Rimuove la classe "active" dall'icona hamburger
     }
 });
